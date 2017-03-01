da66797d2f1db858cd48917ef47246461ecd0638
yangqc <yangqc_ars@outlook.com>
2017-03-01 15:48:43 +0800

    Initial commit

---
+++ src/main/java/com.yqc.test/Example.java
@@ -0,0 +1,26 @@
+package main.java.com.yqc.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by admin on 2017/3/1.
+ */
+
+@RestController
+@EnableAutoConfiguration
+@ComponentScan("com.yqc.test")
+public class Example {
+
+    @RequestMapping("/")
+    String home() {
+        return "Hello World!";
+    }
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(Example.class, args);
+    }
+}
